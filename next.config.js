0ecffcac80a3b4e8b1ebbcb80b1905d97552136b
Govind Nair <noobywinner587@gmail.com>
2025-12-12 16:27:16 +0000

    h2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['three', 'react-globe.gl', 'globe.gl', 'three-globe'],
+}
 
 module.exports = nextConfig
